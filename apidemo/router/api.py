ab772dc511ea4276e6c35a68148b44b7cfd7f5b8
luckyabsoluter <80892490+luckyabsoluter@users.noreply.github.com>
2026-03-26 10:05:24 +0000

    Add thread name log

--- apidemo/router/api.py
+++ apidemo/router/api.py
@@ -1,3 +1,4 @@
+import threading
 import time
 from ninja import NinjaAPI
 
@@ -7,7 +8,9 @@ SLEEP_TIME = 1
 
 @api.get("/sleep")
 def sleep(request):
+    print(f"Received request, sleeping... thread: {threading.current_thread().name}")
     start = time.time()
     time.sleep(SLEEP_TIME)
     end = time.time()
+    print(f"Finished sleeping. thread: {threading.current_thread().name}")
     return {"start": start, "end": end, "duration": end - start}
